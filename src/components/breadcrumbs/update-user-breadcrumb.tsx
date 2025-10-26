69f95508ee6f0149e08236715e2a25219803c5a3
Humberto Ribeiro <betofoxmal@gmail.com>
2025-10-26 00:55:59 -0300

    feat: update breadcrumbs user update

--- src/components/breadcrumbs/update-user-breadcrumb.tsx
+++ src/components/breadcrumbs/update-user-breadcrumb.tsx
@@ -7,11 +7,20 @@ import { type User } from '@/types';
 export default function UpdateUserBreadcrumb({ user }: { user: User }) {
     const { setBreadcrumbs } = useBreadcrumbs();
     useEffect(() => {
-        setBreadcrumbs([
+        const base = [
             { title: 'Painel', href: '/dashboard' },
             { title: 'Administradores', href: '/dashboard/admins' },
-            { title: user?.name ? `Atualizar ${user.name}` : 'Atualizar usuário', href: `/dashboard/admins/update/${user.id}` }
-        ]);
+        ];
+
+        const nameOrDefault = user?.name ? `Atualizar ${user.name}` : 'Atualizar usuário';
+        const updateCrumb = { title: nameOrDefault, href: `/dashboard/admins/update/${user.id}` };
+
+        const crumbs =
+            user.role === 'USER'
+                ? [...base, { title: 'Usuários', href: '/dashboard/admins/users' }, updateCrumb]
+                : [...base, updateCrumb];
+
+        setBreadcrumbs(crumbs);
     }, [setBreadcrumbs, user]);
 
     return null;
