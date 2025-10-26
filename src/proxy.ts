255e545b23049e474dff1c4beb00aef750482201
Humberto Ribeiro <betofoxmal@gmail.com>
2025-10-25 22:36:51 -0300

    feat: ajust proxy type

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { updateSession } from '@/lib/session';
 
 export default async function proxy(req: NextRequest) {
