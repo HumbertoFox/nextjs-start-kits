83d6b924d9e2469b15d42e6727263f6857b009e4
Humberto Ribeiro <betofoxmal@gmail.com>
2025-10-31 21:11:06 -0300

    feat: update redirect not user

--- src/components/breadcrumbs/update-user-breadcrumb.tsx
+++ src/components/breadcrumbs/update-user-breadcrumb.tsx
@@ -3,24 +3,29 @@
 import { useEffect } from 'react';
 import { useBreadcrumbs } from '@/context/breadcrumb-context';
 import { type User } from '@/types';
+import { useRouter } from 'next/navigation';
 
 export default function UpdateUserBreadcrumb({ user }: { user: User }) {
+    const router = useRouter();
     const { setBreadcrumbs } = useBreadcrumbs();
     useEffect(() => {
-        const base = [
-            { title: 'Painel', href: '/dashboard' },
-            { title: 'Administradores', href: '/dashboard/admins' },
-        ];
+        if (user) {
+            const base = [
+                { title: 'Painel', href: '/dashboard' },
+                { title: 'Administradores', href: '/dashboard/admins' },
+            ];
 
-        const nameOrDefault = user?.name ? `Atualizar ${user.name}` : 'Atualizar usuário';
-        const updateCrumb = { title: nameOrDefault, href: `/dashboard/admins/update/${user.id}` };
+            const nameOrDefault = user?.name ? `Atualizar ${user.name}` : 'Atualizar usuário';
+            const updateCrumb = { title: nameOrDefault, href: `/dashboard/admins/update/${user.id}` };
 
-        const crumbs =
-            user.role === 'USER'
-                ? [...base, { title: 'Usuários', href: '/dashboard/admins/users' }, updateCrumb]
-                : [...base, updateCrumb];
+            const crumbs =
+                user.role === 'USER'
+                    ? [...base, { title: 'Usuários', href: '/dashboard/admins/users' }, updateCrumb]
+                    : [...base, updateCrumb];
 
-        setBreadcrumbs(crumbs);
+            setBreadcrumbs(crumbs);
+        }
+        router.back();
     }, [setBreadcrumbs, user]);
 
     return null;
